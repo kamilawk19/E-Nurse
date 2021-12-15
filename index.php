c4ec2aec803f8ce4ffe75db0fb5ddef1c08884b1
jacoboc389 <59968221+jacoboc389@users.noreply.github.com>
2021-12-15 15:52:59 +0100

    system logowania
    tworzenie systemu logowania

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<html lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+
+    <form action="logowanie.php" method="post">
+        Login:
+            <br><input typ="text" name="login"><br>
+        Hasło:
+        <br><input typ="password" name="haslo"><br><br>
+
+        <input type="submit" value="Zaloguj się">
+    </form>
+
+</body>
+
+</html>
+
+
+
