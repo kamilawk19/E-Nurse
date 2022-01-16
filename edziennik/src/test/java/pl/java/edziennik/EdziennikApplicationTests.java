0f41af6649fe9264f3473279c3d4ec6274a72790
Przemek Nosek <przemek.nosek@o2.pl>
2022-01-16 20:10:51 +0100

    initial commit

---
+++ edziennik/src/test/java/pl/java/edziennik/EdziennikApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.java.edziennik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdziennikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
