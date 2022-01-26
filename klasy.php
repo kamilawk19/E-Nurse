2a316be2ef54d5d7040530c5e94a27e69af9b930
jacoboc389 <59968221+jacoboc389@users.noreply.github.com>
2022-01-26 16:59:51 +0100

    Naprawda błędów....

--- klasy.php
+++ klasy.php
@@ -44,7 +44,7 @@
 		
 		<div id="nav">
 			<?php
-				echo '<a href="/front/main.php">Strona główna</a><br>';
+				echo '<a href="front/main.php">Strona główna</a><br>';
 				echo '<a href="klasy.php">Klasy</a><br>';
 				echo '<a href="dziennik.php">Dziennik codzienny</a><br>';
 			?>
